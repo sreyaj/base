b0d60b7a7f39116a98b58d6a7679c939a83b2d4f
Bharath Upadhya <1992bharath@gmail.com>
2016-08-29 19:53:17 +0530

    reindenting to take tab as 2 spaces

--- scripts/remote/installRedis.sh
+++ scripts/remote/installRedis.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
-install_redis(){
-	echo "installing redis"
-	apt-get update && apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y redis-server
+install_redis() {
+  echo "installing redis"
+  apt-get update && apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y redis-server
 }
 
 main() {
-	if [ ! -z "sudo service --status-all 2>&1 | grep redis-server" ]; then
+  if [ ! -z "sudo service --status-all 2>&1 | grep redis-server" ]; then
     echo "Redis already installed, skipping."
     return
   fi
-	install_redis
+  install_redis
 }
 
 main
