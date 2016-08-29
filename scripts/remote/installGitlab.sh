b72b7f72daec8e84f55e84ca38f11d25bfdade81
Bharath Upadhya <1992bharath@gmail.com>
2016-08-29 16:36:22 +0530

    gitlab & vault install scripts

---
+++ scripts/remote/installGitlab.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+
+install_deps() {
+  echo "installing dependencies"
+  sudo apt-get -y install curl openssh-server ca-certificates
+}
+
+install_gitlab() {
+  echo "installing Gitlab"
+  curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+  sudo apt-get -y install gitlab-ce
+}
+
+configure_and_start() {
+  echo "configuring and starting gitlab"
+  sudo gitlab-ctl reconfigure
+}
+
+main() {
+  install_deps
+  install_gitlab
+  configure_and_start
+}
+
+main
