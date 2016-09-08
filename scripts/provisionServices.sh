c81bfc0a34261f41117732a5705715dc4c9cfe75
Bharath Upadhya <1992bharath@gmail.com>
2016-09-08 18:29:57 +0530

    checking state.json if services are already installed & initialized

--- scripts/provisionServices.sh
+++ scripts/provisionServices.sh
@@ -2,6 +2,8 @@
 
 readonly SERVICE_CONFIG="$DATA_DIR/config.json"
 
+export skip_step=0
+
 load_services() {
   # TODO: load service configuration from `config.json`
   local service_count=$(cat $SERVICE_CONFIG | jq '.services | length')
@@ -81,99 +83,10 @@ __map_env_vars() {
   fi
 }
 
-__save_service_config() {
-  local service=$1
-  local ports=$2
-  local opts=$3
-  local component=$4
-  local job_type=$5
-
-  __process_msg "Saving config for $service"
-  local env_vars=$(cat $CONFIG_FILE | jq --arg service "$service" '
-    .services[] |
-    select (.name==$service) | .envs')
-  __process_msg "Found envs for $service: $env_vars"
-
-  local env_vars_count=$(echo $env_vars | jq '. | length')
-  __process_msg "Successfully read from config.json: $service.envs ($env_vars_count)"
-
-  env_values=""
-  for i in $(seq 1 $env_vars_count); do
-    local env_var=$(echo $env_vars | jq -r '.['"$i-1"']')
-    __map_env_vars $env_var $component $job_type
-    env_values="$env_values -e $env_var=$env_value"
-  done
-
-  local state_env=$(cat $STATE_FILE | jq --arg service "$service" '
-    .services  |=
-    map(if .name==$service then
-        .env = "'$env_values'"
-      else
-        .
-      end
-    )'
-  )
-  update=$(echo $state_env | jq '.' | tee $STATE_FILE)
-
-
-  __process_msg "Generating $service replicas"
-  local replicas=$(cat $CONFIG_FILE | jq --arg service "$service" '
-    .services[] |
-    select (.name==$service) | .replicas')
-
-  if [ $replicas != "null" ]; then
-    __process_msg "Found $replicas for $service"
-    local replicas_update=$(cat $STATE_FILE | jq --arg service "$service" '
-      .services  |=
-      map(if .name == $service then
-          .replicas = "'$replicas'"
-        else
-          .
-        end
-      )'
-    )
-    update=$(echo $replicas_update | jq '.' | tee $STATE_FILE)
-    __process_msg "Successfully updated $service replicas"
-  fi
-
-  # Ports
-  __process_msg "Generating $service port mapping"
-  # TODO: Fetch from systemConfig
-  local port_mapping=$ports
-  __process_msg "$service port mapping : $port_mapping"
-
-  if [ ! -z $ports ]; then
-    local port_update=$(cat $STATE_FILE | jq --arg service "$service" '
-      .services  |=
-      map(if .name == $service then
-          .port = "'$port_mapping'"
-        else
-          .
-        end
-      )'
-    )
-    update=$(echo $port_update | jq '.' | tee $STATE_FILE)
-    __process_msg "Successfully updated $service port mapping"
-  fi
-
-  # Opts
-  __process_msg "Generating $service opts"
-  # TODO: Fetch from systemConfig
-  local opts=$3
-  __process_msg "$service opts : $opts"
-
-  if [ ! -z $opts ]; then
-    local opt_update=$(cat $STATE_FILE | jq --arg service "$service" '
-      .services  |=
-      map(if .name == $service then
-          .opts = "'$opts'"
-        else
-          .
-        end
-      )'
-    )
-    update=$(echo $opt_update | jq '.' | tee $STATE_FILE)
-    __process_msg "Successfully updated $service opts"
+_check_component_status() {
+  local status=$(cat $STATE_FILE | jq '.installStatus.'"$1"'')
+  if [ "$status" = true ]; then
+    skip_step=1;
   fi
 }
 
@@ -182,45 +95,153 @@ _update_install_status() {
   _update_state "$update"
 }
 
+__save_service_config() {
+  local service=$1
+  skip_step=0
+  _check_component_status "${service}Initialized"
+  if [ $skip_step -eq 0 ]; then
+    local ports=$2
+    local opts=$3
+    local component=$4
+    local job_type=$5
+
+    __process_msg "Saving config for $service"
+    local env_vars=$(cat $CONFIG_FILE | jq --arg service "$service" '
+      .services[] |
+      select (.name==$service) | .envs')
+    __process_msg "Found envs for $service: $env_vars"
+
+    local env_vars_count=$(echo $env_vars | jq '. | length')
+    __process_msg "Successfully read from config.json: $service.envs ($env_vars_count)"
+
+    env_values=""
+    for i in $(seq 1 $env_vars_count); do
+      local env_var=$(echo $env_vars | jq -r '.['"$i-1"']')
+      __map_env_vars $env_var $component $job_type
+      env_values="$env_values -e $env_var=$env_value"
+    done
+
+    local state_env=$(cat $STATE_FILE | jq --arg service "$service" '
+      .services  |=
+      map(if .name==$service then
+          .env = "'$env_values'"
+        else
+          .
+        end
+      )'
+    )
+    update=$(echo $state_env | jq '.' | tee $STATE_FILE)
+
+
+    __process_msg "Generating $service replicas"
+    local replicas=$(cat $CONFIG_FILE | jq --arg service "$service" '
+      .services[] |
+      select (.name==$service) | .replicas')
+
+    if [ $replicas != "null" ]; then
+      __process_msg "Found $replicas for $service"
+      local replicas_update=$(cat $STATE_FILE | jq --arg service "$service" '
+        .services  |=
+        map(if .name == $service then
+            .replicas = "'$replicas'"
+          else
+            .
+          end
+        )'
+      )
+      update=$(echo $replicas_update | jq '.' | tee $STATE_FILE)
+      __process_msg "Successfully updated $service replicas"
+    fi
+
+    # Ports
+    __process_msg "Generating $service port mapping"
+    # TODO: Fetch from systemConfig
+    local port_mapping=$ports
+    __process_msg "$service port mapping : $port_mapping"
+
+    if [ ! -z $ports ]; then
+      local port_update=$(cat $STATE_FILE | jq --arg service "$service" '
+        .services  |=
+        map(if .name == $service then
+            .port = "'$port_mapping'"
+          else
+            .
+          end
+        )'
+      )
+      update=$(echo $port_update | jq '.' | tee $STATE_FILE)
+      __process_msg "Successfully updated $service port mapping"
+    fi
+
+    # Opts
+    __process_msg "Generating $service opts"
+    # TODO: Fetch from systemConfig
+    local opts=$3
+    __process_msg "$service opts : $opts"
+
+    if [ ! -z $opts ]; then
+      local opt_update=$(cat $STATE_FILE | jq --arg service "$service" '
+        .services  |=
+        map(if .name == $service then
+            .opts = "'$opts'"
+          else
+            .
+          end
+        )'
+      )
+      update=$(echo $opt_update | jq '.' | tee $STATE_FILE)
+      __process_msg "Successfully updated $service opts"
+    fi
+  fi
+}
+
 __run_service() {
   service=$1
-  __process_msg "Provisioning $service on swarm cluster"
-  local swarm_manager_machine=$(cat $STATE_FILE | jq '.machines[] | select (.group=="core" and .name=="swarm")')
-  local swarm_manager_host=$(echo $swarm_manager_machine | jq '.ip')
+  service=$1
+  skip_step=0
+  _check_component_status "${service}Initialized"
+  if [ $skip_step -eq 0 ]; then
+    __process_msg "Provisioning $service on swarm cluster"
+    local swarm_manager_machine=$(cat $STATE_FILE | jq '.machines[] | select (.group=="core" and .name=="swarm")')
+    local swarm_manager_host=$(echo $swarm_manager_machine | jq '.ip')
 
-  local port_mapping=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .port')
-  local env_variables=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .env')
-  local name=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .name')
-  local opts=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .opts')
-  local image=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .image')
-  local replicas=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .replicas')
+    local port_mapping=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .port')
+    local env_variables=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .env')
+    local name=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .name')
+    local opts=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .opts')
+    local image=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .image')
+    local replicas=$(cat $STATE_FILE | jq --arg service "$service" -r '.services[] | select (.name==$service) | .replicas')
 
-  local boot_cmd="sudo docker service create"
+    local boot_cmd="sudo docker service create"
 
-  if [ $port_mapping != "null" ]; then
-    boot_cmd="$boot_cmd $port_mapping"
-  fi
+    if [ $port_mapping != "null" ]; then
+      boot_cmd="$boot_cmd $port_mapping"
+    fi
 
-  if [ $env_variables != "null" ]; then
-    boot_cmd="$boot_cmd $env_variables"
-  fi
+    if [ $env_variables != "null" ]; then
+      boot_cmd="$boot_cmd $env_variables"
+    fi
 
-  if [ $replicas != "null" ]; then
-    boot_cmd="$boot_cmd --replicas $replicas"
+    if [ $replicas != "null" ]; then
+      boot_cmd="$boot_cmd --replicas $replicas"
+    else
+      boot_cmd="$boot_cmd --mode global"
+    fi
+
+    if [ $opts != "null" ]; then
+      boot_cmd="$boot_cmd $opts"
+    fi
+
+    boot_cmd="$boot_cmd $image"
+    _update_install_status "${service}Installed"
+    _update_install_status "${service}Initialized"
+    _exec_remote_cmd "$swarm_manager_host" "docker service rm $service || true"
+    _exec_remote_cmd "$swarm_manager_host" "$boot_cmd"
+    __process_msg "Successfully provisioned $service"
   else
-    boot_cmd="$boot_cmd --mode global"
+    __process_msg "${service} already installed, skipping"
+    __process_msg "${service} already initialized, skipping"
   fi
-
-  if [ $opts != "null" ]; then
-    boot_cmd="$boot_cmd $opts"
-  fi
-
-  boot_cmd="$boot_cmd $image"
-  _update_install_status "${service}Installed"
-  _update_install_status "${service}Initialized"
-  _exec_remote_cmd "$swarm_manager_host" "docker service rm $service || true"
-  _exec_remote_cmd "$swarm_manager_host" "$boot_cmd"
-  __process_msg "Successfully provisioned $service"
 }
 
 provision_www() {
