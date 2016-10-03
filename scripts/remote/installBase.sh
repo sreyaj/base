b1f0f5ad2adca58308c52451cb9309b332ceae6d
Bharath Upadhya <1992bharath@gmail.com>
2016-10-03 17:32:38 +0530

    fixing warnings

--- scripts/remote/installBase.sh
+++ scripts/remote/installBase.sh
@@ -1,32 +1,38 @@
 #!/bin/bash -e
 
-add_keys() {
+update_sources() {
+  update_sources_docker
+  update_sources_rabbitmq
+  update_sources_postgresql
+}
+
+update_sources_docker() {
   apt-get install -y apt-transport-https ca-certificates
   apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-  wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-  apt-key add rabbitmq-signing-key-public.asc
-
-  wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
-}
-
-update_sources() {
-  rabbitmq_deb="deb http://www.rabbitmq.com/debian/ testing main"
-  cat /etc/apt/sources.list | grep $rabbitmq_deb
-  exit_status=$?
-  if [ exit_status -eq 1 ]; then
-    echo $rabbitmq_deb >> /etc/apt/sources.list
-  fi
   docker_deb="deb https://apt.dockerproject.org/repo ubuntu-trusty main"
-  cat /etc/apt/sources.list.d/docker.list | grep $docker_deb
-  exit_status=$?
-  if [ exit_status -eq 1 ]; then
+  docker=$(cat /etc/apt/sources.list.d/docker.list 2>/dev/null | grep "$docker_deb") || true
+  if [ -z "$docker" ]; then
     echo $docker_deb | tee -a /etc/apt/sources.list.d/docker.list
   fi
+}
+
+update_sources_rabbitmq() {
+  wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
+
+  rabbitmq_deb="deb http://www.rabbitmq.com/debian/ testing main"
+  rabbitmq=$(cat /etc/apt/sources.list 2>/dev/null | grep "$rabbitmq_deb") || true
+  if [ -z "$rabbitmq" ]; then
+    echo $rabbitmq_deb >> /etc/apt/sources.list
+  fi
+}
+
+update_sources_postgresql() {
+  wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
+
   postgresql_deb="deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main"
-  cat /etc/apt/sources.list.d/pgdg.list | grep $postgresql_deb
-  exit_status=$?
-  if [ exit_status -eq 1 ]; then
+  postgres=$(cat /etc/apt/sources.list.d/pgdg.list 2>/dev/null | grep "$postgresql_deb") || true
+  if [ -z "$postgres" ]; then
     echo $postgresql_deb | tee -a /etc/apt/sources.list.d/pgdg.list
   fi
 }
@@ -37,7 +43,6 @@ install_base_binaries() {
 }
 
 main() {
-  add_keys
   update_sources
   install_base_binaries
 }
