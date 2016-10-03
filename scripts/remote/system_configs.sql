fdefb040c4e8f982876b89b76bd3b5222d4830d4
Shrivara K S <shrivara.jois@gmail.com>
2016-10-03 13:15:08 +0530

    Update systemConfigs to be alter table for new columns

--- scripts/remote/system_configs.sql
+++ scripts/remote/system_configs.sql
@@ -1,30 +1,44 @@
-create table if not exists "systemConfigs" (
-  "id" INT  PRIMARY KEY NOT NULL,
-  "defaultMinionCount" INT DEFAULT 1 NOT NULL,
-  "defaultPipelineCount" INT DEFAULT 1 NOT NULL,
-  "serverEnabled" BOOLEAN NOT NULL,
-  "cachingEnabled" BOOLEAN DEFAULT TRUE NOT NULL,
-  "hubspotEnabled" BOOLEAN DEFAULT TRUE NOT NULL,
-  "buildTimeoutMS" INT DEFAULT 3600000 NOT NULL,
-  "defaultPrivateJobQuota" INT DEFAULT 150 NOT NULL,
-  "serviceUserToken" VARCHAR(36) NOT NULL,
-  "vaultUrl" VARCHAR(255),
-  "vaultToken" VARCHAR(45),
-  "vaultRefreshTimeInSec" INT,
-  "amqpUrl" VARCHAR(255),
-  "amqpUrlRoot" VARCHAR(255),
-  "amqpUrlAdmin" VARCHAR(255),
-  "amqpDefaultExchange" VARCHAR(255),
-  "apiUrl" VARCHAR(255),
-  "apiPort" INT,
-  "wwwUrl" VARCHAR(255),
-  "runMode" VARCHAR(255) DEFAULT 'debug' NOT NULL,
-  "rootQueueList" TEXT,
-  "execImage" VARCHAR(255) NOT NULL,
-  "createdAt" timestamp with time zone NOT NULL,
-  "updatedAt" timestamp with time zone NOT NULL,
-  "dynamicNodesSystemIntegrationId" VARCHAR(24),
-  "systemNodePrivateKey" TEXT,
-  "systemNodePublicKey" VARCHAR(1020)
-);
-alter table "systemConfigs" owner to "apiuser";
+do $$
+  begin
+    create table if not exists "systemConfigs" (
+      "id" INT  PRIMARY KEY NOT NULL,
+      "defaultMinionCount" INT DEFAULT 1 NOT NULL,
+      "defaultPipelineCount" INT DEFAULT 1 NOT NULL,
+      "serverEnabled" BOOLEAN NOT NULL,
+      "cachingEnabled" BOOLEAN DEFAULT TRUE NOT NULL,
+      "hubspotEnabled" BOOLEAN DEFAULT TRUE NOT NULL,
+      "buildTimeoutMS" INT DEFAULT 3600000 NOT NULL,
+      "defaultPrivateJobQuota" INT DEFAULT 150 NOT NULL,
+      "serviceUserToken" VARCHAR(36) NOT NULL,
+      "vaultUrl" VARCHAR(255),
+      "vaultToken" VARCHAR(45),
+      "vaultRefreshTimeInSec" INT,
+      "amqpUrl" VARCHAR(255),
+      "amqpUrlRoot" VARCHAR(255),
+      "amqpUrlAdmin" VARCHAR(255),
+      "amqpDefaultExchange" VARCHAR(255),
+      "apiUrl" VARCHAR(255),
+      "apiPort" INT,
+      "wwwUrl" VARCHAR(255),
+      "runMode" VARCHAR(255) DEFAULT 'debug' NOT NULL,
+      "rootQueueList" TEXT,
+      "execImage" VARCHAR(255) NOT NULL,
+      "createdAt" timestamp with time zone NOT NULL,
+      "updatedAt" timestamp with time zone NOT NULL
+    );
+
+    if not exists (select 1 from information_schema.columns where table_name = 'systemConfigs' and column_name = 'dynamicNodesSystemIntegrationId') then
+      alter table "systemConfigs" add column "dynamicNodesSystemIntegrationId" VARCHAR(24);
+    end if;
+
+    if not exists (select 1 from information_schema.columns where table_name = 'systemConfigs' and column_name = 'systemNodePrivateKey') then
+      alter table "systemConfigs" add column "systemNodePrivateKey" TEXT;
+    end if;
+
+    if not exists (select 1 from information_schema.columns where table_name = 'systemConfigs' and column_name = 'systemNodePublicKey') then
+      alter table "systemConfigs" add column "systemNodePublicKey" VARCHAR(1020);
+    end if;
+
+    alter table "systemConfigs" owner to "apiuser";
+  end
+$$;
