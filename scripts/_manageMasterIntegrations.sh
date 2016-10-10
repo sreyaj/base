294bef177c832888f660bce3a166419976f715a8
Devashish <devashish.86@gmail.com>
2016-10-10 11:12:33 -0700

    getting the list of all master integrations from db

--- scripts/_manageMasterIntegrations.sh
+++ scripts/_manageMasterIntegrations.sh
@@ -7,7 +7,25 @@ export DISABLED_MASTER_INTEGRATIONS=""
 get_available_masterIntegrations() {
   __process_msg "GET-ing available master integrations from db"
   # GET MI list from DB update global variable
-  true
+
+  local api_url=""
+  local api_token=$(cat $STATE_FILE | jq -r '.systemSettings.serviceUserToken')
+  local api_url=$(cat $STATE_FILE | jq -r '.systemSettings.apiUrl')
+  local master_integrations_get_endpoint="$api_url/masterIntegrations"
+
+  local response=$(curl -H "Content-Type: application/json" -H "Authorization: apiToken $api_token" \
+    -X GET $master_integrations_get_endpoint \
+    --silent)
+  response=$(echo $response | jq '.')
+  local response_length=$(echo $response | jq '. | length')
+
+  if [ $response_length -gt 5 ]; then
+    ## NOTE: we're assuming we have at least 5 master integrations in global list
+    __process_msg "Successfully fetched master integration list: $response_length"
+  else
+    local error=$(echo $response | jq '.')
+    __process_msg "Error GET-ing master integration list: $error"
+  fi
 }
 
 validate_masterIntegrations(){
