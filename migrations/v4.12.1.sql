b6c64e997406e31d653a60029b68208c7bac2aea
mohit5it9 <mohit@shippable.com>
2016-11-24 11:31:36 +0530

    adds routeRoles for triggerNewBuildRequest

--- migrations/v4.12.1.sql
+++ migrations/v4.12.1.sql
@@ -7361,6 +7361,24 @@ do $$
       roleCode := 6060
     );
 
+    perform set_route_role(
+      routePattern := '/projects/:projectId/newBuild',
+      httpVerb := 'POST',
+      roleCode := 6010
+    );
+
+    perform set_route_role(
+      routePattern := '/projects/:projectId/newBuild',
+      httpVerb := 'POST',
+      roleCode := 6020
+    );
+
+    perform set_route_role(
+      routePattern := '/projects/:projectId/newBuild',
+      httpVerb := 'POST',
+      roleCode := 6060
+    );
+
     -- set projectAccounts routeRoles
 
     perform set_route_role(
