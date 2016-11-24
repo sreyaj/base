1ea5a1ede4168654baa212db56acebceb54fc20c
Devashish <devashish.86@gmail.com>
2016-11-23 17:16:14 -0800

    using replica count from state file

--- scripts/_manageServices.sh
+++ scripts/_manageServices.sh
@@ -5,19 +5,43 @@ release_file="$VERSIONS_DIR/$RELEASE_VERSION".json
 __add_service() {
   local service_name="$1"
 
-  local service=$(cat $release_file | jq -c '.serviceConfigs[] | select (.name=='$service_name')')
+  local service=$(cat $release_file \
+    | jq -c '.serviceConfigs[] | select (.name=='$service_name')')
+
   if [ ! -z "$service" ]; then
-    service=$(cat $STATE_FILE | jq '.services[] | select (.name=='$service_name')')
-    if [ -z "$service" ]; then
+    __process_msg "service $service_name present in release file"
+    local service_state=$(cat $STATE_FILE \
+      | jq '.services[] | select (.name=='$service_name')')
+
+    if [ -z "$service_state" ]; then
       __process_msg "no $service_name service in state.json, creating new one"
-      service=$(cat $STATE_FILE |  \
-        jq '.services |= . + [{
-          "name": '$service_name',
-          "image": "",
-          "env": ""
-        }]')
-      update=$(echo $service | jq '.' | tee $STATE_FILE)
+      local is_service_global=$(echo $service \
+        | jq -r '.isGlobal')
+      if [ "$is_service_global" == true ]; then
+        __process_msg "service $service_name is global, not setting default replica count"
+        service_state=$(cat $STATE_FILE |  \
+          jq '.services |= . + [{
+            "name": '$service_name',
+            "image": "",
+            "env": ""
+          }]')
+      else
+        __process_msg "service $service_name not global, setting default replica count"
+        service_state=$(cat $STATE_FILE |  \
+          jq '.services |= . + [{
+            "name": '$service_name',
+            "image": "",
+            "env": "",
+            "replicas": 1
+          }]')
+      fi
+      update=$(echo $service_state | jq '.' | tee $STATE_FILE)
+    else
+      __process_msg "service $service_name already present in state file"
     fi
+  else
+    __process_msg "Error!!! service $service_name not present in release file"
+    exit 1
   fi
 }
 
@@ -44,11 +68,26 @@ add_master_integration_services() {
   done
 }
 
+configure_replicas() {
+  __process_msg "Configuring replicas for services"
+  local replicas_configured=$(cat $STATE_FILE | jq -r '.installStatus.replicasConfigured')
+
+  if [ $replicas_configured == true ]; then
+    __process_msg "Service replicas already configured, skipping"
+  else
+    __process_msg "Service replicas configuration required"
+    __process_msg "Please set the desired replica count in 'services[]' array in 'usr/state.json' file"
+    __process_msg "Once completed, set 'installStatus.replicasConfigure=true' and re-run installer"
+    exit 1
+  fi
+}
+
 main() {
   __process_marker "Configuring services list"
 
   add_core_services
   add_master_integration_services
+  configure_replicas
   __process_msg "Configured services list"
 }
 
